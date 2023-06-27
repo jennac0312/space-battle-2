beb991f8c9a967286c8336f26be6b26934b2b666
Jenna C <jennac0312@gmail.com>
2023-06-27 13:04:10 -0400

    moving fwd. new direction

---
+++ ideas.js
@@ -0,0 +1,11 @@
+// rick enemies pics:
+
+
+// story book muscle guy
+// https://media3.giphy.com/media/LrR2zZm8nhSk9CPLo5/giphy.gif?cid=6c09b952791609927d6671418511d693a1d2f2c20edb3afd&ep=v1_internal_gifs_gifId&rid=giphy.gif&ct=s
+
+// screaming sun not transparent
+// https://media.tenor.com/yalDO5GyNA8AAAAC/screaming-sun-rick-and-morty.gif
+
+// 
+
